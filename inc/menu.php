7b6b2c14dc2f51271dd709b4029aeefc1437240f
quantumsp <minosh@mail.ru>
2018-07-13 19:17:39 +0600

    menu

--- inc/menu.php
+++ inc/menu.php
@@ -3,6 +3,13 @@
     <nav id="mainheader">
       <ul>
         <li class="logo">Game</li>
+        <li class="search"><input type="text" name="search" placeholder="Search..."/><div class="searchb"></div></li>
+        <li><a href="#">Help</a></li>
+        <li><a href="#">Register</a></li>
+        <li><a href="#">Login</a></li>
+        <li id="basket"> <div class="bicon"></div> My Basket
+          <div class="basket"><span>Items in my basket</span> <br><center>0 Item(s)</center> </div>
+        </li>
       </ul>
     </nav>
   </header>
