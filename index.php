3f04a50909e4d4589d68921701340402cca4945d
quantumsp <minosh@mail.ru>
2018-07-13 11:53:21 +0600

    ++

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php $ptitle = "Game | Home"?>
-<?php require_once $_SERVER["DOCUMENT_ROOT"]. '/project/inc/head.php'; ?>
-<?php require_once $_SERVER["DOCUMENT_ROOT"]. '/project/inc/footer.php'; ?>
-<?php require_once $_SERVER["DOCUMENT_ROOT"]. '/project/inc/menu.php'; ?>
+<?php require_once $_SERVER["DOCUMENT_ROOT"]. '/developeurskill/inc/head.php'; ?>
+<?php require_once $_SERVER["DOCUMENT_ROOT"]. '/developeurskill/inc/footer.php'; ?>
+<?php require_once $_SERVER["DOCUMENT_ROOT"]. '/developeurskill/inc/menu.php'; ?>
