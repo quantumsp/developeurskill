1b6c61471f077db5a7350bbc5b74bd4f05d0ea1f
quantumsp <minosh@mail.ru>
2018-07-16 20:07:30 +0600

    ++

--- inc/head.php
+++ inc/head.php
@@ -6,7 +6,11 @@
     <meta name="keywords" content="Frivolite">
     <title><?php print $ptitle?></title>
     <link rel="stylesheet" href="css/style.css" />
-    <!-- <link rel="stylesheet" href="css/blueberry.css" /> -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="css/jquery.bxslider.css" />
+    <link href="css/font-awesome.min.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+    <script src="https://code.jquery.com/jquery-3.1.1.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
   </head>
   <body>
